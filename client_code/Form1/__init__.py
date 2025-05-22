368a5f77285f7875901652abf737317a7663f06f
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-22 13:17:13 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from datetime import date
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,6 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def date_picker_2_change(self, **event_args):
+    self.label_1.text = self.date_picker_2.date - self.date_picker_1.date
